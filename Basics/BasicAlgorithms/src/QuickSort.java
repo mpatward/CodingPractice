244471d8080c561de284e5fd731c610b58eceaa5
mpatward <patwardhan.mandar@gmail.com>
2015-10-19 15:51:56 -0700

    Quick Sort
    Implemented quick sort with randomized partition.

--- Basics/BasicAlgorithms/src/QuickSort.java
+++ Basics/BasicAlgorithms/src/QuickSort.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+import java.util.Random;
 
 /**
  * @author mandar
@@ -8,16 +6,31 @@
  *Time complexity: O(n*log n) in average case. and O(n^2) in worst case. Its very fast and is an In place sort.
  *
  */
+
 public class QuickSort {
 
 	/**
-	 * @param args
+	 * Returns a pseudo-random number between min and max, inclusive.
 	 */
+	public static int randInt(int min, int max) {
+	    Random rand=new Random();
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+	    return randomNum;
+	}
 	public static void swap(int array[],int i,int pIndex){
 		int temp=array[i];
 		array[i]=array[pIndex];
 		array[pIndex]=temp;
 	}
+	
+	public static void randomizedPartition(int array[],int start,int end){
+		int pIndex= randInt(start, end);
+		swap(array, pIndex, end);
+		partition(array, start, end);
+	}
+	
 	public static int partition(int array[],int start,int end){
 		int pivotElement=array[end];
 		int pIndex=start;
@@ -30,6 +43,7 @@ public class QuickSort {
 		swap(array, pIndex, end);
 		return pIndex;
 	}
+	
 	public static void quickSort(int array[],int start,int end){
 		if(start<end){
 			int pIndex=partition(array, start, end);
@@ -37,6 +51,7 @@ public class QuickSort {
 			quickSort(array,pIndex+1,end);
 		}
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int array[]={11,2,33,4,65,36,37,187,92,420};
@@ -45,5 +60,4 @@ public class QuickSort {
 			System.out.println(array[i]);
 		}
 	}
-
 }
